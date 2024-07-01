ba80682b77e885abd6e524abe95d230f216ab305
Galers <galosaandrew@gmail.com>
2024-07-01 16:17:58 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+/* eslint-disable function-paren-newline */ "use strict";
+const reg = /N/g;
+const divFields = [
+    ...document.querySelectorAll(".field")
+];
+const inputs = divFields.flatMap((elem)=>Array.from(elem.querySelectorAll("input")));
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    label.classList.add("field-label");
+    label.setAttribute("for", input.id);
+    const inputName = input.name.replace(reg, " N");
+    label.textContent = inputName;
+    const placeholderText = `${inputName.charAt(0).toUpperCase()}${inputName.slice(1)}`;
+    input.setAttribute("placeholder", placeholderText);
+    input.parentNode.insertBefore(label, input);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
